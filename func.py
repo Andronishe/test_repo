06396d3c666df7fc510b570bbba20d337b72811f
Andrey Matskevich <wedas018@mail.ru>
2022-05-19 20:37:44 +0300

    added tmp2 fuction

--- func.py
+++ func.py
@@ -3,9 +3,14 @@ def spam():
     эта функция производит самаризацию
     :return:
     '''
+    c =+ 1
+    print(c)
     print('*' * 20)
     print('samara' * 20)
 
 
 def tmp():
+    return 54
+
+def tmp2():
     return 54
